44971d7c851b995c80553a189d5f4ff07b0bb632
darioaplicano <alex_dario92@hotmail.com>
2015-10-14 21:20:46 -0600

    Agregado formulario datos generales y transporte

--- pages/Neonatos/pacientes/forms_registro/form_datosGenerales.php
+++ pages/Neonatos/pacientes/forms_registro/form_datosGenerales.php
@@ -1,10 +1,112 @@
 <div class="row">
-    <div class="col-md-12">
-      <div class="featurette">
-
-            <img class="featurette-image  pull-left" src="img/angel.png">
-            <div class="alert alert-success"><h1>(formulario 1) Datos Generales</h1></div>
-       
-      </div>
+    <label class="col-xs-6 control-label">
+        Institución de nacimiento:
+        <input type="text" class="form-control" id="institucion_nacimiento">
+    </label>
+    <label class="col-xs-6 control-label">
+        Lugar de nacimiento:
+        <input type="text" class="form-control" id="lugar_nacimiento">
+    </label>
+</div>
+<div class="row">
+    <label class="col-xs-12 control-label">
+        Nombre del recien nacido:
+        <input type="text" class="form-control" id="nombre_recien_nacido">
+    </label>
+</div>
+<div class="row">
+    <div class="col-xs-6">
+        <label class="control-label">Tiene pulsera ID: </label>
+        <label class="radio-inline"><input type="radio" name="optradio"
+                                           id="tiene_pulsera_si">Si</label>
+        <label class="radio-inline"><input type="radio" name="optradio"
+                                           id="tiene_pulsera_no">No</label> 
+    </div>
+    <div class="col-xs-6">
+        <label class="control-label">Sexo: </label>
+        <label class="radio-inline"><input type="radio" name="optradio1">H</label>
+        <label class="radio-inline"><input type="radio" name="optradio1">M</label>
+        <label class="radio-inline"><input type="radio" name="optradio1">No determinado</label> 
+    </div>
+</div>
+<div class="row">
+    <label class="col-xs-6 control-label">
+        Nombre de la madre:
+        <input type="text" class="form-control">
+    </label>
+    <label class="col-xs-6 control-label">
+        Historia clínica de la madre:
+        <input type="text" class="form-control">
+    </label>
+</div>
+<div class="row">
+    <div class="col-xs-5">
+        <label class="control-label">
+            Ubicación de la madre:
+        </label>
+        <div class="col-xs-6">
+            <label class="radio"><input type="radio" name="optradio1">Mismo Hospital</label>
+            <label class="radio"><input type="radio" name="optradio1">Otro Hospital</label>
+        </div>
+        <div class="col-xs-6">
+            <label class="radio"><input type="radio" name="optradio1">Domicilio</label>
+            <label class="radio"><input type="radio" name="optradio1">Fallecida</label>
+        </div>
+    </div>
+    <div class="col-xs-6">
+        <div class="col-xs-6">
+            <label class="control-label">
+                Condición de salud:
+            </label>
+            <div class="col-xs-6">
+                <label class="radio"><input type="radio" name="optradio1">Normal</label>
+            </div>
+            <div class="col-xs-6">
+                <label class="radio"><input type="radio" name="optradio1">Crítica</label>
+            </div>
+        </div>
+        <div class="col-xs-6">
+            <label class="control-label">
+                Conoció a su hijo(a):
+            </label>
+            <div class="col-xs-6">
+                <label class="radio"><input type="radio" name="optradio1">Si</label>
+            </div>
+            <div class="col-xs-6">
+                <label class="radio"><input type="radio" name="optradio1">No</label>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <label class="col-xs-12 control-label">
+        Nombre del padre o responsable (parentesco):
+        <input type="text" class="form-control" id="nombre_recien_nacido">
+    </label>
+</div>
+<div class="row">
+    <div class="col-xs-5">
+        <label class="col-xs-6 control-label">
+            Dirección:
+            <input type="text" class="form-control" id="institucion_nacimiento">
+        </label>
+        <label class="col-xs-6 control-label">
+            Teléfono:
+            <input type="text" class="form-control" id="lugar_nacimiento">
+        </label>
+    </div>
+    <div class="col-xs-7">
+            <label class="control-label col-xs-12">
+                Información inicial de la familia:
+            </label>
+            <div class="col-xs-4">
+                <label class="radio"><input type="radio" name="optradio1">Directa</label>
+            </div>
+            <div class="col-xs-4">
+                <label class="radio"><input type="radio" name="optradio1">Telefónica</label>
+            </div>
+            <div class="col-xs-4">
+                <label class="radio"><input type="radio" name="optradio1">No se logró</label>
+            </div>
     </div>
 </div>
